d1a47be13960c2e3b1ee91b788a2ed04eb425d6a
Piyavadee Pariyavuth <piyavadeepariyavuth@gmail.com>
2018-03-06 14:36:16 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,6 +11,45 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  array[1..2].sort {|a,b| b <=> a}
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |x|
+    split = x.split("")
+    split[2] = "$"
+    new_array << (split.join)
+  end
+  new_array
+end
+
+def find_a(array)
+  new_array = []
+  array.each do |x|
+    if x.start_with?("a")
+      new_array << x
+    end
+  end
+  new_array
+end
+
+def sum_array(array)
+  array.inject {|result, element| result + element}
+end
+
+def add_s(array)
+  array.collect do |x|
+    if x == "feet"
+      x
+    else
+      x << ("s")
+    end
+  end
   array
 end
